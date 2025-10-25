f017120d57444aa11abe8946c304d252616b410a
Gomez Azul <azulbgomez@gmail.com>
2025-10-25 16:07:05 -0300

    no se que es?

---
+++ unqui-po2-gomez-tp3/test/ar/edu/unq/PersonaTestCase.java
@@ -0,0 +1,27 @@
+package ar.edu.unq;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonaTestCase {
+	
+	private Persona azul;
+	
+	@BeforeEach
+	public void setUp() {
+		azul = new Persona("Azul", LocalDate.of(2004, 2, 20));
+	}
+
+	@Test
+	void testSePuedeSaberLosDatosDeUnaPersona() {
+		
+		assertEquals("Azul", azul.getNombre());
+		assertEquals(LocalDate.of(2004, 2, 20), azul.getFechaDeNacimiento());
+		assertEquals(21, azul.edad());
+	}
+
+}
